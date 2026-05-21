4349217013e1e08bc53808fbb2eb66f07554d494
”abdelrahman <eabdo6990@gmail.com>
2026-05-21 16:15:13 +0300

    center the div and make folder structure

---
+++ react todo/src/Component/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Home.css";
+export default function Home() {
+  return (
+    <div className="bigdiv">
+      <div className="content">
+        <h1>REACT TODO APP</h1>
+      </div>
+    </div>
+  );
+}
