07787ca18c6800b37ea2d6dee4705ace3393f183
”abdelrahman <eabdo6990@gmail.com>
2026-05-21 19:49:53 +0300

    add function

--- react todo/src/Component/Home.jsx
+++ react todo/src/Component/Home.jsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css";
 export default function Home() {
+  let [input, setInput] = useState("");
+  let [todo, setTodo] = useState([]);
+  function Add() {
+    if (input.trim() === "") return;
+    setTodo([...todo, input]);
+    setInput("");
+  }
+  let items = [];
+  for (let i = 0; i < todo.length; i++) {
+    items.push(<li key={i}>{todo[i]}</li>);
+  }
   return (
     <div className="bigdiv">
       <div className="content">
         <h1>REACT TODO APP</h1>
+        <input
+          type="text"
+          placeholder="Add a new todo"
+          className="input"
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+        />
+        <button className="btn" onClick={Add}>
+          Add
+        </button>
+        <ul>{items}</ul>
       </div>
     </div>
   );
