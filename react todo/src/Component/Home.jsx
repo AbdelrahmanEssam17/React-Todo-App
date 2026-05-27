46b2f9e203bcd45757667a4e5b10b6438a2be0a7
”abdelrahman <eabdo6990@gmail.com>
2026-05-27 14:56:03 +0300

    add update function

--- react todo/src/Component/Home.jsx
+++ react todo/src/Component/Home.jsx
@@ -3,27 +3,27 @@ import "./Home.css";
 export default function Home() {
   let [input, setInput] = useState("");
   let [todo, setTodo] = useState([]);
+  let [updated, setUpdated] = useState(false);
+  let [editIndex, setEditIndex] = useState(null);
+
   function Add() {
     if (input.trim() === "") return;
-    setTodo([...todo, input]);
+    if (updated) {
+      let allupdated = [...todo];
+      allupdated[editIndex] = input;
+      setTodo(allupdated);
 
+      setUpdated(false);
+      setEditIndex(null);
+    } else {
+      setTodo([...todo, input]);
+    }
     setInput("");
   }
-  let items = [];
-  for (let i = 0; i < todo.length; i++) {
-    items.push(
-      <li key={i}>
-        {todo[i]}
-        <button
-          className="dbtn"
-          onClick={() => {
-            deleted(i);
-          }}
-        >
-          deleted
-        </button>
-      </li>,
-    );
+  function update(index) {
+    setInput(todo[index]);
+    setUpdated(true);
+    setEditIndex(index);
   }
   function deleted(index) {
     let undeleted = [];
@@ -34,6 +34,21 @@ export default function Home() {
     }
     setTodo(undeleted);
   }
+  let items = [];
+  for (let i = 0; i < todo.length; i++) {
+    items.push(
+      <li key={i}>
+        {todo[i]}
+        <button className="dbtn" onClick={() => deleted(i)}>
+          deleted
+        </button>
+
+        <button className="updatebtn" onClick={() => update(i)}>
+          Updated
+        </button>
+      </li>,
+    );
+  }
   return (
     <div className="bigdiv">
       <div className="content">
@@ -43,12 +58,14 @@ export default function Home() {
           placeholder="Add a new todo"
           className="input"
           value={input}
-          onChange={(e) => {
-            setInput(e.target.value);
-          }}
+          onChange={(e) => setInput(e.target.value)}
         />
-        <button className="btn" onClick={Add}>
-          Add
+        <button
+          className="btn"
+          onClick={Add}
+          style={{ backgroundColor: updated ? "yellow" : "blue" }}
+        >
+          {updated ? "update" : "Add"}
         </button>
         <ul>{items}</ul>
       </div>
